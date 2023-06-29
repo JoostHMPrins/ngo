9fbf9f83566b31c48348ba48df49a14db91ab1a2
prins <j.h.m.prins@student.tue.nl>
2023-06-29 16:40:50 +0200

    Data generation folder

---
+++ datageneration/heatconduction2d.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from nutils import mesh, function, solver
+from nutils.expression_v2 import Namespace
+
+from randompolynomials import randompoly1DO3, randompoly2DO3
