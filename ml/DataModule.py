65f4a7e321743ad8c854540fbea8146144d3357e
prins <j.h.m.prins@student.tue.nl>
2023-07-05 17:35:04 +0200

    Changed hparams to hyperparams

--- ml/DataModule.py
+++ ml/DataModule.py
@@ -12,7 +12,7 @@ class DataModule(pl.LightningDataModule):
         super().__init__()
         
         self.data_dir = data_dir
-        self.params = hparams
+        self.hyperparams = hparams
 
     def setup(self, stage=None):
         self.Theta = np.load(self.data_dir + '/Theta.npy')
@@ -20,17 +20,17 @@ class DataModule(pl.LightningDataModule):
         self.N = np.load(self.data_dir + '/N.npy')
         self.x = np.load(self.data_dir + '/x.npy')
         self.u = np.load(self.data_dir + '/u.npy')
-        self.Theta = torch.tensor(self.Theta, dtype=self.params['dtype'])
-        self.F = torch.tensor(self.F, dtype=self.params['dtype'])
-        self.N = torch.tensor(self.N, dtype=self.params['dtype'])
-        self.x = torch.tensor(self.x, dtype=self.params['dtype'])
-        self.u = torch.tensor(self.u, dtype=self.params['dtype'])
+        self.Theta = torch.tensor(self.Theta, dtype=self.hyperparams['dtype'])
+        self.F = torch.tensor(self.F, dtype=self.hyperparams['dtype'])
+        self.N = torch.tensor(self.N, dtype=self.hyperparams['dtype'])
+        self.x = torch.tensor(self.x, dtype=self.hyperparams['dtype'])
+        self.u = torch.tensor(self.u, dtype=self.hyperparams['dtype'])
         
         self.dataset = torch.utils.data.TensorDataset(self.Theta, self.F, self.N, self.x, self.u)
         self.trainingset, self.validationset = random_split(self.dataset, [int(0.9*self.u.shape[0]), int(0.1*self.u.shape[0])])
 
     def train_dataloader(self):
-        return DataLoader(self.trainingset, batch_size=self.params['batch_size'])
+        return DataLoader(self.trainingset, batch_size=self.hyperparams['batch_size'])
 
     def val_dataloader(self):
-        return DataLoader(self.validationset, batch_size=self.params['batch_size'])
+        return DataLoader(self.validationset, batch_size=self.hyperparams['batch_size'])
