a6649f1998fc551c1c7b4573e80d17455d44350c
prins <j.h.m.prins@student.tue.nl>
2023-07-05 17:31:20 +0200

    Debugged, data flows correctly through VarMiON

--- ml/VarMiON.py
+++ ml/VarMiON.py
@@ -1,6 +1,5 @@
 import torch
 from torch import nn
-from torch.nn import functional as F
 import pytorch_lightning as pl
 
 
@@ -15,7 +14,7 @@ class GaussianRBF(nn.Module):
         nn.init.constant_(self.log_sigmas, 0)
         
     def forward(self, x):
-        d_scaled = (x[:,None,:] - self.mus[None,:,:]/torch.exp(self.log_sigmas[None,:,None]))
+        d_scaled = (x[:,:,None,:] - self.mus[None,None,:,:]/torch.exp(self.log_sigmas[None,None,:,None]))
         return torch.exp(-(torch.linalg.vector_norm(d_scaled, axis=-1, ord=2))**2/2)
     
     
@@ -35,13 +34,15 @@ class NLBranchNet(nn.Module):
         self.layers.append(nn.ReLU())
 
     def forward(self, x):
+        x = x.unsqueeze(1)
         for layer in self.layers:
             x = layer(x)
+        x = x.squeeze()
         return x
 
     
 class LBranchNet(nn.Module):
-    def __init__(self, input_dim, output_dim):
+    def __init__(self, input_dim=100, output_dim=72):
         super().__init__()
         self.layers = nn.ModuleList()
         self.layers.append(nn.Linear(input_dim, output_dim))
@@ -55,30 +56,41 @@ class LBranchNet(nn.Module):
 class VarMiON(pl.LightningModule):
     def __init__(self, params):
         super().__init__()
+        self.params = params
         self.hparams.update(params['hparams'])
-
+        self.NLBranch = NLBranchNet()
+        self.LBranch = LBranchNet(144,72)
+        self.Trunk = GaussianRBF(2,72)
+        
     def forward(self, Theta, F, N, x):
-        NLBranch = NLBranchNet().forward(Theta)
-        NLBranch = NLBranch.reshape((NLBranch.shape[0], NLBranch.shape[2], NLBranch.shape[3]))
-        LBranch = LBranchNet(144,72).forward(F) + LBranchNet(144,72).forward(N)
+        NLBranch = self.NLBranch.forward(Theta)
+        LBranch = self.LBranch.forward(F) + self.LBranch.forward(N)
         Branch = torch.einsum('nij,nj->ni', NLBranch, LBranch)
-        Trunk = GaussianRBF(2,72).forward(x)
-        u_hat = torch.einsum('ni,ni->n', Branch, Trunk)
+        Trunk = self.Trunk.forward(x)
+        u_hat = torch.einsum('ni,nbi->nb', Branch, Trunk)
         return u_hat
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        optimizer = self.hparams['optimizer'](self.parameters(), lr=self.hparams['learning_rate'])
         return optimizer
 
     def training_step(self, train_batch, batch_idx):
         Theta, F, N, x, u = train_batch
-        u_hat = VarMiON().forward(Theta, F, N, x)
-        loss = F.mse_loss(u_hat, u)
+        u_hat = self.forward(Theta, F, N, x)
+        loss = nn.functional.mse_loss(u_hat, u)
+        # loss = 0
+        # for i in range(len(self.hparams['loss_coeffs'])):
+        #     loss = loss + self.hparams['loss_coeffs'][i]*self.hparams['loss_terms'][i](self, u_hat, u)
+        # loss = loss/sum(self.hparams['loss_coeffs'])
         self.log('train_loss', loss)
         return loss
 
     def validation_step(self, val_batch, batch_idx):
         Theta, F, N, x, u = val_batch
-        u_hat = VarMiON().forward(Theta, F, N, x)
-        loss = F.mse_loss(u_hat, u)
+        u_hat = self.forward(Theta, F, N, x)
+        loss = nn.functional.mse_loss(u_hat, u)
+        # loss = 0
+        # for i in range(len(self.hparams['loss_coeffs'])):
+        #     loss = loss + self.hparams['loss_coeffs'][i]*self.hparams['loss_terms'][i](self, u_hat, u)
+        # loss = loss/sum(self.hparams['loss_coeffs'])
         self.log('val_loss', loss)
