0c29ab2ac4e239f57a77b3f96fa8885755624c4e
prins <j.h.m.prins@student.tue.nl>
2023-07-05 17:32:07 +0200

    Works, except for logger and callbacks

--- ml/trainer.py
+++ ml/trainer.py
@@ -2,14 +2,15 @@ import pytorch_lightning as pl
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 from tqdm.auto import tqdm, trange
 from ipywidgets import IntProgress
+import json
 import time
 from DataModule import *
 from logger import *
 
-def loadandtrain(model, hparams, loaddir, label, sublogdir):
+def train(model, hparams, loaddir, label, sublogdir):
     
     #Initialize logger and checkpointer
-    logger, checkpoint_callback = initializelogger(label, sublogdir)
+    #logger, checkpoint_callback = initializelogger(label, sublogdir)
     
     #Load data and params
     data = DataModule(loaddir, hparams)
@@ -19,19 +20,20 @@ def loadandtrain(model, hparams, loaddir, label, sublogdir):
     params['label'] = label
 
     #Model
-    model = model(hparams)
-    model = model.to(hparams['dtype'])
-    model.params = params
+    MLmodel = model(params)
+    MLmodel = MLmodel.to(hparams['dtype'])
+    MLmodel.params = params
     
     #Training
     start = time.time() 
-    trainer = pl.Trainer(logger=logger, 
-                         gpus=hparams['gpus'],
-                         precision=hparams['precision'], 
-                         max_epochs=hparams['max_epochs'], 
-                         check_val_every_n_epoch=1,
-                         callbacks=[checkpoint_callback, EarlyStopping(monitor='val_loss', patience=hparams['early_stopping_patience'])])
-    trainer.fit(model, data)
+    trainer = pl.Trainer(accelerator="gpu", devices=[0], max_epochs=20)
+    # trainer = pl.Trainer(logger=logger, 
+    #                      gpus=hparams['gpus'],
+    #                      precision=hparams['precision'], 
+    #                      max_epochs=hparams['max_epochs'], 
+    #                      check_val_every_n_epoch=1,
+    #                      callbacks=[checkpoint_callback, EarlyStopping(monitor='val_loss', patience=hparams['early_stopping_patience'])])
+    trainer.fit(MLmodel, data)
     end = time.time()
     Ctime = end - start #computation time
     print("Training time: %fs"%Ctime)
