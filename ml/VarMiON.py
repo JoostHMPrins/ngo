d9313f3a52ebc56a798393cec47edce684cb55ed
prins <j.h.m.prins@student.tue.nl>
2023-07-19 14:12:32 +0200

    Added simforward, added VarMiON changes/additions as hparams

--- ml/VarMiON.py
+++ ml/VarMiON.py
@@ -6,68 +6,65 @@ from torch_rbf import RBF, gaussian
 
 
 class GaussianRBF(nn.Module):
-    def __init__(self, input_dim, output_dim):
+    def __init__(self, params, input_dim, output_dim):
         super().__init__()
         #Definition and initialization of centers and scales
-        self.mus = torch.nn.Parameter(torch.ones(output_dim, input_dim))
-        self.log_sigmas = torch.nn.Parameter(torch.ones(output_dim))
+        self.hparams = params['hparams']
+        self.mus = nn.Parameter(torch.Tensor(output_dim, input_dim))
+        self.log_sigmas = nn.Parameter(torch.Tensor(output_dim))
         self.reset_parameters()
         
     def reset_parameters(self):
         nn.init.uniform_(self.mus, 0, 1)
-        nn.init.constant_(self.log_sigmas, -1)
+        nn.init.constant_(self.log_sigmas, np.log(0.15))
         
     def forward(self, x):
-        d_scaled = ((x[:,:,None,:] - self.mus[None,None,:,:])/torch.exp(self.log_sigmas[None,None,:,None]))
-        output = torch.exp(-1/2*(torch.linalg.vector_norm(d_scaled, axis=-1, ord=2))**2)
-        return torch.exp(-1/2*(torch.linalg.vector_norm(d_scaled, axis=-1, ord=2))**2)
+        d_scaled = (x[:,:,None,:] - self.mus[None,None,:,:])/torch.exp(self.log_sigmas[None,None,:,None])
+        y = torch.exp(-torch.sum(d_scaled**2, axis=-1))
+        if self.hparams.get('norm_basis',False)==True:
+            y = y/torch.sum(y, axis=-1)[:,:,None]
+        return y
     
     
 class NLBranchNet(nn.Module):
-    def __init__(self):
+    def __init__(self, params):
         super().__init__()
+        self.hparams = params['hparams']
         self.layers = nn.ModuleList()
-        self.layers.append(nn.ConvTranspose2d(in_channels=1, out_channels=16, kernel_size=4, stride=1))
+        self.layers.append(nn.ConvTranspose2d(in_channels=1, out_channels=16, kernel_size=4, stride=1, bias=self.hparams.get('bias_NLBranch',True)))
         self.layers.append(nn.ReLU())
         self.layers.append(nn.BatchNorm2d(num_features=16))
-        self.layers.append(nn.ConvTranspose2d(in_channels=16, out_channels=32, kernel_size=4, stride=1))
+        self.layers.append(nn.ConvTranspose2d(in_channels=16, out_channels=32, kernel_size=4, stride=1, bias=self.hparams.get('bias_NLBranch',True)))
         self.layers.append(nn.ReLU())
         self.layers.append(nn.BatchNorm2d(num_features=32))
-        self.layers.append(nn.ConvTranspose2d(in_channels=32, out_channels=16, kernel_size=2, stride=2))
+        self.layers.append(nn.ConvTranspose2d(in_channels=32, out_channels=16, kernel_size=2, stride=2, bias=self.hparams.get('bias_NLBranch',True)))
         self.layers.append(nn.ReLU())
-        self.layers.append(nn.ConvTranspose2d(in_channels=16, out_channels=1, kernel_size=2, stride=2))
-        self.layers.append(nn.ReLU())
+        self.layers.append(nn.ConvTranspose2d(in_channels=16, out_channels=1, kernel_size=2, stride=2, bias=self.hparams.get('bias_NLBranch',True)))
+        if self.hparams.get('NLB_outputReLU',False)==True:
+            self.layers.append(nn.ReLU())
 
     def forward(self, x):
         x = x.unsqueeze(1)
         for layer in self.layers:
             x = layer(x)
-        x = x[0]
-        return x
+        y = x.squeeze()
+        if self.hparams.get('symmetrize_D',False)==True:
+            y = 1/2*(y + y.transpose(-1,-2))
+        return y
 
     
 class LBranchNet(nn.Module):
-    def __init__(self, input_dim, output_dim):
+    def __init__(self, params, input_dim, output_dim):
         super().__init__()
+        self.hparams = params['hparams']
         self.layers = nn.ModuleList()
-        self.layers.append(nn.Linear(input_dim, output_dim))
+        self.layers.append(nn.Linear(input_dim, output_dim, bias=self.hparams.get('bias_LBranch',True)))
 
     def forward(self, x):
         for layer in self.layers:
             x = layer(x)
-        return x
-    
-
-class TrunkNet(nn.Module):
-    def __init__(self, input_dim, output_dim, basis_func):
-        super().__init__()
-        self.layers = nn.ModuleList()
-        self.layers.append(RBF(in_features=input_dim, out_features=output_dim, basis_func=basis_func))
-
-    def forward(self, x):
-        for layer in self.layers:
-            x = layer(x)
-        return x
+            y = x
+        return y
     
     
 class VarMiON(pl.LightningModule):
@@ -75,16 +72,17 @@ class VarMiON(pl.LightningModule):
         super().__init__()
         self.params = params
         self.hparams.update(params['hparams'])
-        self.NLBranch = NLBranchNet()
-        self.LBranchF = LBranchNet(input_dim=144, output_dim=72)
-        self.LBranchN = LBranchNet(input_dim=144, output_dim=72)
+        self.NLBranch = NLBranchNet(params)
+        self.LBranchF = LBranchNet(params, input_dim=144, output_dim=72)
+        #self.LBranchN = LBranchNet(input_dim=144, output_dim=72)
         # self.Trunk = TrunkNet(input_dim=2, output_dim=72, basis_func=gaussian)
         # self.Trunk = RBF(2,72,gaussian)
-        self.Trunk = GaussianRBF(2,72)
+        self.Trunk = GaussianRBF(params, input_dim=2, output_dim=72)
+        self = self.to(self.hparams['dtype'])
         
     def forward(self, Theta, F, N, x):
         NLBranch = self.NLBranch.forward(Theta)
-        LBranch = self.LBranchF.forward(F) + self.LBranchN.forward(N)
+        LBranch = self.LBranchF.forward(F) #+ self.LBranchN.forward(N)
         Branch = torch.einsum('nij,nj->ni', NLBranch, LBranch)
         Trunk = self.Trunk.forward(x)
         u_hat = torch.einsum('ni,noi->no', Branch, Trunk)
@@ -94,15 +92,16 @@ class VarMiON(pl.LightningModule):
         Theta = torch.tensor(Theta, dtype=self.hparams['dtype'])
         Theta = Theta.unsqueeze(0)
         F = torch.tensor(F, dtype=self.hparams['dtype'])
-        F = F.unsqueeze(0)
         N = torch.tensor(N, dtype=self.hparams['dtype'])
-        N = N.unsqueeze(0)
         x = torch.tensor(x, dtype=self.hparams['dtype'])
-        x = x.unsqueeze(0)
-        u = self.forward(Theta, F, N, x)
+        x = x.unsqueeze(1).unsqueeze(1)
+        NLBranch = self.NLBranch.forward(Theta)
+        LBranch = self.LBranchF.forward(F) #+ self.LBranchN.forward(N)
+        Branch = torch.einsum('ij,j->i', NLBranch, LBranch)
+        Trunk = self.Trunk.forward(x).squeeze()
+        u = torch.einsum('i,oi->o', Branch, Trunk)
         u = torch.detach(u).cpu()
         u = np.array(u)
-        u = u[0]
         return u
 
     def configure_optimizers(self):
@@ -112,24 +111,20 @@ class VarMiON(pl.LightningModule):
     def training_step(self, train_batch, batch_idx):
         Theta, F, N, x, u = train_batch
         u_hat = self.forward(Theta, F, N, x)
-        eps = 2**(-52)
-        loss = nn.functional.mse_loss(u_hat, u)
-        # loss = 0
-        # for i in range(len(self.hparams['loss_coeffs'])):
-        #     loss = loss + self.hparams['loss_coeffs'][i]*self.hparams['loss_terms'][i](self, u_hat, u)
-        # loss = loss/sum(self.hparams['loss_coeffs'])
+        loss = 0
+        for i in range(len(self.hparams['loss_coeffs'])):
+            loss = loss + self.hparams['loss_coeffs'][i]*self.hparams['loss_terms'][i](u_hat, u)
+        loss = loss/sum(self.hparams['loss_coeffs'])
         self.log('train_loss', loss)
         return loss
 
     def validation_step(self, val_batch, batch_idx):
         Theta, F, N, x, u = val_batch
         u_hat = self.forward(Theta, F, N, x)
-        eps = 2**(-52)
-        loss = nn.functional.mse_loss(u_hat, u)
-        # loss = 0
-        # for i in range(len(self.hparams['loss_coeffs'])):
-        #     loss = loss + self.hparams['loss_coeffs'][i]*self.hparams['loss_terms'][i](self, u_hat, u)
-        # loss = loss/sum(self.hparams['loss_coeffs'])
+        loss = 0
+        for i in range(len(self.hparams['loss_coeffs'])):
+            loss = loss + self.hparams['loss_coeffs'][i]*self.hparams['loss_terms'][i](u_hat, u)
+        loss = loss/sum(self.hparams['loss_coeffs'])
         self.log('val_loss', loss)
         metric = self.hparams['metric'](u_hat, u)
         self.log('metric', metric)
