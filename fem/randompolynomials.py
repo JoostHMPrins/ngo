e58bf51f5f5ec817ec3f15984e5314113417db2e
prins <j.h.m.prins@student.tue.nl>
2023-07-19 14:12:04 +0200

    Set offset of positive polynomials to 0.5

--- fem/randompolynomials.py
+++ fem/randompolynomials.py
@@ -22,7 +22,7 @@ def randompoly2DO3(c):
 def randompoly1DO3sqr(c):
     
     def polynomial(x):
-        poly = (c[0] + c[1]*x + c[2]*x**2 + c[3]*x**3)**2
+        poly = 0.5 + (c[0] + c[1]*x + c[2]*x**2 + c[3]*x**3)**2
         return poly
         
     return polynomial
@@ -31,7 +31,7 @@ def randompoly1DO3sqr(c):
 def randompoly2DO3sqr(c):
     
     def polynomial(x,y):
-        poly = 0.1 + (c[0] + c[1]*x + c[2]*y + c[3]*x**2 + c[4]*y**2 + c[5]*x*y + c[6]*x**3 + c[7]*y**3 + c[8]*x*y**2 + c[9]*y*x**2)**2
+        poly = 0.5 + (c[0] + c[1]*x + c[2]*y + c[3]*x**2 + c[4]*y**2 + c[5]*x*y + c[6]*x**3 + c[7]*y**3 + c[8]*x*y**2 + c[9]*y*x**2)**2
         return poly
     
     return polynomial
