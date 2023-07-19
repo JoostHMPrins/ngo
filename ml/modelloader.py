e18c532f45a3a570360ea524979b23756f1582c7
prins <j.h.m.prins@student.tue.nl>
2023-07-19 14:12:52 +0200

    removed unnecessary lines

--- ml/modelloader.py
+++ ml/modelloader.py
@@ -7,11 +7,9 @@ def loadmodelfromlabel(model, label, logdir, sublogdir, map_location):
         if file.endswith('.ckpt'):
             ckpt = torch.load(logdir + '/' + sublogdir + '/' + label + '/' + file,  map_location=map_location)
             hparams = ckpt['hyper_parameters']
-            Model = model(ckpt['params'])
-            Model = Model.to(ckpt['hyper_parameters']['dtype'])
+            params = ckpt['params']
+            Model = model(params)
             statedict = ckpt['state_dict']
             Model.load_state_dict(statedict)
-            Model.params = ckpt['params']
-            Model.label = label
                     
     return Model
