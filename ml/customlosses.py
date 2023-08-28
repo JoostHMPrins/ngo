8cb730860e94cc4c61683a1c2952a6b07a1de86d
prins <j.h.m.prins@student.tue.nl>
2023-08-28 17:25:58 +0200

    Added scaled L2 as metric

---
+++ ml/customlosses.py
@@ -0,0 +1,10 @@
+import torch
+from torch.nn import functional as F
+
+def L2_scaled(u_hat, u):
+    L2 = torch.norm(u_hat - u, p=2, dim=-1)
+    norm = torch.norm(u, p=2, dim=-1)
+    return torch.mean(L2/norm)
+
+
+    
