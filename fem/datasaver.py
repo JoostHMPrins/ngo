6238fceaabde9b7e6d70ae2504ae526bedaf1633
prins <j.h.m.prins@student.tue.nl>
2023-12-12 17:38:09 +0100

    Restructuring training data generation code

--- fem/datasaver.py
+++ fem/datasaver.py
@@ -1,14 +1,32 @@
 import numpy as np
 import os
 import json
+import dill
 
-def savedata(params, data, savedir, label):
-    
-    if not os.path.isdir(savedir+'/'+label):
-        os.makedirs(savedir+'/'+label)
-        
-    with open(savedir+'/'+label+'/params.json', 'w') as fp:
+
+# def savedata(params, data, savedir, label):
+#     if not os.path.isdir(savedir+'/'+label):
+#         os.makedirs(savedir+'/'+label)
+#     with open(savedir+'/'+label+'/params.json', 'w') as fp:
+#         json.dump(params, fp)
+#     for key, value in data.items() :
+#         np.save(savedir+'/'+label+'/'+key+'.npy', value)
+
+def savedata(params, variable, data, savedir):
+    if not os.path.isdir(savedir):
+        os.makedirs(savedir)
+    with open(savedir+'/params.json', 'w') as fp:
         json.dump(params, fp)
-    
-    for key, value in data.items() :
-        np.save(savedir+'/'+label+'/'+key+'.npy', value)
+    np.save(savedir+'/'+variable+'.npy', value)
+        
+        
+def save_function_list(params, variable, function_list, savedir):
+    if not os.path.isdir(savedir):
+        os.makedirs(savedir)
+    with open(savedir+'/params.json', 'w') as fp:
+        json.dump(params, fp)
+    # Serialize the function list
+    serialized_functions = dill.dumps(function_list)
+    # Save the serialized functions to a file
+    with open(savedir+'/'+variable+'.pkl', 'wb') as file:
+        file.write(serialized_functions)
