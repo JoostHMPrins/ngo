d493e7de8573962c0f68d8b607192a4264480176
prins <j.h.m.prins@student.tue.nl>
2024-01-02 16:58:27 +0100

    Added input and output function discretization

--- ml/DataModule.py
+++ ml/DataModule.py
@@ -4,8 +4,12 @@ import torch
 from torch.utils.data import DataLoader
 from torch.utils.data import random_split
 import pytorch_lightning as pl
+
+import sys
+sys.path.insert(0, '/home/prins/st8/prins/phd/gitlab/ngo-pde-gk/fem') 
+from datasaver import load_function_list
     
-    
+
 class DataModule_hc2d(pl.LightningDataModule):
 
     def __init__(self, data_dir, hparams):
@@ -15,18 +19,44 @@ class DataModule_hc2d(pl.LightningDataModule):
         self.hyperparams = hparams
 
     def setup(self, stage=None):
-        self.Theta = np.load(self.data_dir + '/Theta.npy')
-        self.F = np.load(self.data_dir + '/F.npy')
-        self.N = np.load(self.data_dir + '/N.npy')
-        self.x = np.load(self.data_dir + '/x.npy')
-        self.u = np.load(self.data_dir + '/u.npy')
-        self.Theta = torch.tensor(self.Theta, dtype=self.hyperparams['dtype'])
-        self.F = torch.tensor(self.F, dtype=self.hyperparams['dtype'])
-        self.N = torch.tensor(self.N, dtype=self.hyperparams['dtype'])
+        #Load and discretize input and output functions
+        print('Preprocessing data...')
+        theta = load_function_list(variable='theta', loaddir=self.data_dir)
+        f = load_function_list(variable='f', loaddir=self.data_dir)
+        etat = load_function_list(variable='etat', loaddir=self.data_dir)
+        etab = load_function_list(variable='etab', loaddir=self.data_dir)
+        x = np.load(self.data_dir + '/x.npy')
+        u = np.load(self.data_dir + '/u.npy')
+        #Define quadrature grid
+        x_0, x_1 = np.mgrid[0:1:self.hyperparams['Q']*1j, 0:1:self.hyperparams['Q']*1j]
+        x_Q = np.vstack([x_0.ravel(), x_1.ravel()]).T
+        #Define empty lists for data
+        self.theta = []
+        self.f = []
+        self.etab = []
+        self.etat = []
+        self.x = []
+        self.u = []
+        #Discretize input functions and sample output data
+        for i in range(len(theta)):
+            self.theta.append(theta[i](x_Q))
+            self.f.append(f[i](x_Q))
+            self.etab.append(etab[i](x_Q))
+            self.etat.append(etat[i](x_Q))
+            indices = np.linspace(0,x.shape[1]-1, x.shape[1], dtype=int)
+            indices_output = np.random.choice(indices, size=self.hyperparams['Q_L'], replace=False)
+            self.x.append(x[i,indices_output])
+            self.u.append(u[i,indices_output])
+        
+        #Convert to torch tensors
+        self.theta = torch.tensor(self.theta, dtype=self.hyperparams['dtype']).reshape(len(theta),self.hyperparams['Q'],self.hyperparams['Q'])
+        self.f = torch.tensor(self.f, dtype=self.hyperparams['dtype']).reshape(len(theta),self.hyperparams['Q'],self.hyperparams['Q'])
+        self.etab = torch.tensor(self.etab, dtype=self.hyperparams['dtype']).reshape(len(theta),self.hyperparams['Q'],self.hyperparams['Q'])
+        self.etat = torch.tensor(self.etat, dtype=self.hyperparams['dtype']).reshape(len(theta),self.hyperparams['Q'],self.hyperparams['Q'])
         self.x = torch.tensor(self.x, dtype=self.hyperparams['dtype'])
         self.u = torch.tensor(self.u, dtype=self.hyperparams['dtype'])
         
-        self.dataset = torch.utils.data.TensorDataset(self.Theta, self.F, self.N, self.x, self.u)
+        self.dataset = torch.utils.data.TensorDataset(self.theta, self.f, self.etab, self.etat, self.x, self.u)
         self.trainingset, self.validationset = random_split(self.dataset, [int(0.9*self.u.shape[0]), int(0.1*self.u.shape[0])])
 
     def train_dataloader(self):
@@ -34,6 +64,36 @@ class DataModule_hc2d(pl.LightningDataModule):
 
     def val_dataloader(self):
         return DataLoader(self.validationset, batch_size=self.hyperparams['batch_size'])
+
+
+# class DataModule_hc2d_old(pl.LightningDataModule):
+
+#     def __init__(self, data_dir, hparams):
+#         super().__init__()
+        
+#         self.data_dir = data_dir
+#         self.hyperparams = hparams
+
+#     def setup(self, stage=None):
+#         self.Theta = np.load(self.data_dir + '/Theta.npy')
+#         self.F = np.load(self.data_dir + '/F.npy')
+#         self.N = np.load(self.data_dir + '/N.npy')
+#         self.x = np.load(self.data_dir + '/x.npy')
+#         self.u = np.load(self.data_dir + '/u.npy')
+#         self.Theta = torch.tensor(self.Theta, dtype=self.hyperparams['dtype'])
+#         self.F = torch.tensor(self.F, dtype=self.hyperparams['dtype'])
+#         self.N = torch.tensor(self.N, dtype=self.hyperparams['dtype'])
+#         self.x = torch.tensor(self.x, dtype=self.hyperparams['dtype'])
+#         self.u = torch.tensor(self.u, dtype=self.hyperparams['dtype'])
+        
+#         self.dataset = torch.utils.data.TensorDataset(self.Theta, self.F, self.N, self.x, self.u)
+#         self.trainingset, self.validationset = random_split(self.dataset, [int(0.9*self.u.shape[0]), int(0.1*self.u.shape[0])])
+
+#     def train_dataloader(self):
+#         return DataLoader(self.trainingset, batch_size=self.hyperparams['batch_size'])
+
+#     def val_dataloader(self):
+#         return DataLoader(self.validationset, batch_size=self.hyperparams['batch_size'])
     
     
 class DataModule_bm(pl.LightningDataModule):
