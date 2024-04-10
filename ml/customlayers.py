e7c90d779eb3b6887708225fbb87472c244243f6
prins <j.h.m.prins@student.tue.nl>
2024-04-10 10:03:45 +0200

    Added non-trainable parameters to RBFs

--- ml/customlayers.py
+++ ml/customlayers.py
@@ -7,15 +7,23 @@ class GaussianRBF(nn.Module):
     def __init__(self, params, input_dim, output_dim):
         super().__init__()
         self.params = params
+        self.input_dim = input_dim
+        self.output_dim = output_dim
         self.hparams = params['hparams']
-        self.mus = nn.Parameter(torch.Tensor(output_dim, input_dim))
-        self.log_sigmas = nn.Parameter(torch.Tensor(output_dim))
-        self.reset_parameters()
+        self.reset_nontrainableparameters()
         
     def reset_parameters(self):
+        self.mus = nn.Parameter(torch.Tensor(self.output_dim, self.input_dim))
+        self.log_sigmas = nn.Parameter(torch.Tensor(self.output_dim))
         nn.init.uniform_(self.mus, 0, 1)
         nn.init.constant_(self.log_sigmas, np.log((1/self.hparams['h'])**(1/self.params['simparams']['d'])))
-        
+                                     
+    def reset_nontrainableparameters(self):
+        mu_0, mu_1 = np.mgrid[0:1:self.hparams['h']*1j, 0:1:self.hparams['h']*1j]
+        mus = np.vstack([mu_0.ravel(), mu_1.ravel()]).T
+        self.mus = torch.tensor(mus)
+        self.log_sigmas = torch.ones(self.output_dim)*np.log((1/self.hparams['h']))               
+                                     
     def forward(self, x):
         if self.hparams.get('symgroupavg',False)==True:
             mus_temp = torch.einsum('ij,gj->gi', self.mapping, self.mus - 1/2) + 1/2
