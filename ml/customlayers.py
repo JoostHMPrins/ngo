393fd00f03415015abc1fa554eeaa581598483aa
prins <j.h.m.prins@student.tue.nl>
2024-04-18 15:15:12 +0200

    Added fixed RBFs

--- ml/customlayers.py
+++ ml/customlayers.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+import torch.nn.functional as F
 import numpy as np
 
 
@@ -11,6 +12,7 @@ class GaussianRBF(nn.Module):
         self.output_dim = output_dim
         self.hparams = params['hparams']
         self.reset_nontrainableparameters()
+        # self.reset_parameters()
         
     def reset_parameters(self):
         self.mus = nn.Parameter(torch.Tensor(self.output_dim, self.input_dim))
@@ -19,29 +21,29 @@ class GaussianRBF(nn.Module):
         nn.init.constant_(self.log_sigmas, np.log((1/self.hparams['h'])**(1/self.params['simparams']['d'])))
                                      
     def reset_nontrainableparameters(self):
-        mu_0, mu_1 = np.mgrid[0:1:self.hparams['h']*1j, 0:1:self.hparams['h']*1j]
+        mu_0, mu_1 = np.mgrid[0:1:int(self.hparams['h']**(1/self.params['simparams']['d']))*1j, 0:1:int(self.hparams['h']**(1/self.params['simparams']['d']))*1j]        
         mus = np.vstack([mu_0.ravel(), mu_1.ravel()]).T
         self.mus = torch.tensor(mus)
-        self.log_sigmas = torch.ones(self.output_dim)*np.log((1/self.hparams['h']))               
+        self.log_sigmas = torch.ones(self.output_dim)*np.log((1/self.hparams['h']**(1/self.params['simparams']['d'])))               
                                      
     def forward(self, x):
         if self.hparams.get('symgroupavg',False)==True:
             mus_temp = torch.einsum('ij,gj->gi', self.mapping, self.mus - 1/2) + 1/2
-            d_scaled = (x[:,:,None,:] - mus_temp[None,None,:,:])/torch.exp(self.log_sigmas[None,None,:,None])
+            d_scaled = (x[:,None,:] - mus_temp[None,:,:])/torch.exp(self.log_sigmas[None,:,None])
         else:
-            d_scaled = (x[:,:,None,:] - self.mus[None,None,:,:])/torch.exp(self.log_sigmas[None,None,:,None])
+            d_scaled = (x[:,None,:] - self.mus[None,:,:])/torch.exp(self.log_sigmas[None,:,None])
         y = torch.exp(-torch.sum(d_scaled**2, axis=-1)/2)
         if self.hparams.get('norm_basis',False)==True:
-            y = y/torch.sum(y, axis=-1)[:,:,None]
+            y = y/torch.sum(y, axis=-1)[:,None]
         return y
     
     def grad(self, x):
         if self.hparams.get('symgroupavg',False)==True:
             mus_temp = torch.einsum('ij,gj->gi', self.mapping, self.mus - 1/2) + 1/2
-            prefactor = -1/(torch.exp(self.log_sigmas[None,None,:,None]))**2*(x[:,:,None,:] - mus_temp[None,None,:,:])
+            prefactor = -1/(torch.exp(self.log_sigmas[None,:,None]))**2*(x[:,None,:] - mus_temp[None,:,:])
         else:
-            prefactor = -1/(torch.exp(self.log_sigmas[None,None,:,None]))**2*(x[:,:,None,:] - self.mus[None,None,:,:])
-        return prefactor*self.forward(x)[:,:,:,None]
+            prefactor = -1/(torch.exp(self.log_sigmas[None,:,None]))**2*(x[:,None,:] - self.mus[None,:,:])
+        return prefactor*self.forward(x)[:,:,None]
 
         
 class GaussianRBF_NOMAD(nn.Module):
@@ -59,6 +61,28 @@ class GaussianRBF_NOMAD(nn.Module):
         if self.hparams.get('norm_basis',False)==True:
             y = y/torch.sum(y, axis=-1)[:,:,None]
         return y
+    
+
+class ReshapeLayer(nn.Module):
+    def __init__(self, output_shape):
+        super().__init__()
+        self.output_shape = output_shape
+    
+    def forward(self, x):
+        y = x.reshape(self.output_shape)
+        return y
+
+    
+class UpsampleModel(nn.Module):
+    def __init__(self, size):
+        super(UpsampleModel, self).__init__()
+        self.size = size
+        
+    def forward(self, x):
+        x = F.interpolate(x, size=self.size, mode='nearest')
+        return x
+    
+
     
 
 def expand_D8(A):
