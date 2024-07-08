b225af20da780271b9b5a55c277636e79920293c
prins <j.h.m.prins@student.tue.nl>
2024-07-08 14:29:12 +0200

    Added quadrature based loss

--- ml/customlosses.py
+++ ml/customlosses.py
@@ -6,4 +6,18 @@ def L2_scaled(u_hat, u):
     norm = torch.norm(u, p=2, dim=-1)
     return torch.mean(L2/norm)
 
-    
+def weightedL2(w, u_hat, u):
+    L22 = torch.sum(w[None,:]*(u_hat - u)**2, axis=-1)
+    L2 = L22**(1/2)
+    return torch.mean(L2)
+
+def weightedL2squared(w, u_hat, u):
+    L22 = torch.sum(w[None,:]*(u_hat - u)**2, axis=-1)
+    return torch.mean(L22)
+
+def weightedrelativeL2(w, u_hat, u):
+    L22_diff = torch.sum(w[None,:]*(u_hat - u)**2, axis=-1)
+    L2_diff = L22_diff**(1/2)
+    L22_norm = torch.sum(w[None,:]*(u)**2, axis=-1)
+    L2_norm = L22_norm**(1/2)
+    return torch.mean(L2_diff/torch.maximum(L2_norm, 1e-7*torch.ones_like(L2_norm)))
