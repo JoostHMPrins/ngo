41dcbbb300485de3288e4735299be65185a4552e
prins <j.h.m.prins@student.tue.nl>
2024-09-13 17:40:19 +0200

    Added function discretizer, parameter balancer

--- ml/customlayers.py
+++ ml/customlayers.py
@@ -4,9 +4,30 @@ import torch.nn.functional as F
 import numpy as np
 
 
-def discretize_functions(f_list, x):
-    f_list = np.array(f_list)
-    f_discretized = np.zeros((len(f_list),x.shape[0]))
+def balance_num_trainable_params(model, N_w):
+    model.free_parameter = 1
+    N_w_real_0 = model.hparams['N_w_real']
+    free_parameter_values_list = []
+    N_w_real = N_w_real_0
+    while N_w_real < N_w:
+        model.init_layers()
+        N_w_real = N_w_real_0 + sum(p.numel() for p in model.parameters())
+        free_parameter_values_list.append(model.free_parameter)
+        model.free_parameter +=1
+    if len(free_parameter_values_list)>1:
+        model.free_parameter = free_parameter_values_list[-2]
+    else:
+        model.free_parameter = free_parameter_values_list[0]
+    model.init_layers()
+    count = sum(p.numel() for p in model.parameters())
+    model.hparams['N_w_real'] += count
+    if model.hparams['N_w_real']>N_w:
+        raise ValueError('Not enough trainable parameter budget.')
+    return model  
+
+
+def discretize_functions(f_list, x, device):
+    f_discretized = torch.zeros((len(f_list),x.shape[0]), device=device)
     for i in range(len(f_list)):
         print(i)
         f_discretized[i] = f_list[i](x)
