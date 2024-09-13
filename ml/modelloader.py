8c21db11adb7c70533b29c732eddfffd008fef5e
prins <j.h.m.prins@student.tue.nl>
2024-09-13 17:40:40 +0200

    Changed map location

--- ml/modelloader.py
+++ ml/modelloader.py
@@ -6,10 +6,10 @@ def loadmodelfromlabel(model, label, logdir, sublogdir, map_location):
     for file in os.listdir(logdir + '/' + sublogdir + '/' + label):
         if file.endswith('.ckpt'):
             ckpt = torch.load(logdir + '/' + sublogdir + '/' + label + '/' + file,  map_location=map_location)
-            hparams = ckpt['hyper_parameters']
-            params = ckpt['params']
-            Model = model(params)
+            hparams = ckpt['hparams']
+            Model = model(hparams)
             statedict = ckpt['state_dict']
             Model.load_state_dict(statedict)
-            Model.device = map_location
+            Model.used_device = map_location
+            Model = Model.to(Model.used_device)
     return Model
