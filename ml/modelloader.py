3e3365ba4844d59a3da75cf50cb63abebdea5e69
prins <j.h.m.prins@student.tue.nl>
2024-11-18 10:04:14 +0100

    Fixed device loading

--- ml/modelloader.py
+++ ml/modelloader.py
@@ -7,9 +7,9 @@ def loadmodelfromlabel(model, label, logdir, sublogdir, map_location):
         if file.endswith('.ckpt'):
             ckpt = torch.load(logdir + '/' + sublogdir + '/' + label + '/' + file,  map_location=map_location)
             hparams = ckpt['hparams']
+            hparams['used_device'] = map_location
             Model = model(hparams)
             statedict = ckpt['state_dict']
             Model.load_state_dict(statedict)
-            Model.used_device = map_location
             Model = Model.to(Model.used_device)
     return Model
