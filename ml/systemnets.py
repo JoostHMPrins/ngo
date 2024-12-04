72190a37968e7d4923a263ca200d4333bbbd51ec
prins <j.h.m.prins@student.tue.nl>
2024-12-04 17:37:12 +0100

    Added CNNNd

--- ml/systemnets.py
+++ ml/systemnets.py
@@ -113,6 +113,77 @@ class CNN(nn.Module):
         return y
     
 
+class CNNNd(nn.Module):
+    def __init__(self, hparams):
+        super().__init__()
+        self.hparams = hparams
+        self = balance_num_trainable_params(model=self, N_w=self.hparams['N_w'])
+        self = self.to(torch.device(self.hparams['used_device']))
+
+    def init_layers(self):
+        self.layers = nn.ModuleList()
+        self.kernel_sizes = self.hparams['kernel_sizes']
+        self.bottleneck_size = self.hparams['bottleneck_size']
+        #Layers
+        self.layers.append(ReshapeLayer(output_shape=self.hparams['input_shape']))
+        self.layers.append(ConvNd(in_channels=self.hparams['input_shape'][0], out_channels=self.free_parameter, kernel_size=self.kernel_sizes[0], stride=self.kernel_sizes[0], bias=False))
+        self.layers.append(nn.ReLU())
+        self.layers.append(ConvNd(in_channels=self.free_parameter, out_channels=self.free_parameter, kernel_size=self.kernel_sizes[1], stride=self.kernel_sizes[1], bias=False))
+        self.layers.append(nn.ReLU())
+        self.layers.append(ConvNd(in_channels=self.free_parameter, out_channels=self.free_parameter, kernel_size=self.kernel_sizes[2], stride=self.kernel_sizes[2], bias=False))
+        self.layers.append(nn.ReLU())
+        self.layers.append(ConvNd(in_channels=self.free_parameter, out_channels=self.bottleneck_size, kernel_size=self.kernel_sizes[3], stride=self.kernel_sizes[3], bias=False))
+        self.layers.append(ReshapeLayer(output_shape=(int(self.bottleneck_size),)))
+        self.layers.append(nn.Linear(in_features=int(self.bottleneck_size), out_features=int(self.bottleneck_size)))
+        self.layers.append(nn.ReLU())
+        self.layers.append(ReshapeLayer(output_shape=(self.bottleneck_size,1,1)))
+        self.layers.append(nn.ConvTranspose2d(self.bottleneck_size, self.free_parameter, kernel_size=self.kernel_sizes[4], stride=self.kernel_sizes[4], bias=False))
+        self.layers.append(nn.ReLU())
+        self.layers.append(nn.ConvTranspose2d(self.free_parameter, self.free_parameter, kernel_size=self.kernel_sizes[5], stride=self.kernel_sizes[5], bias=False))
+        self.layers.append(nn.ReLU())
+        self.layers.append(nn.ConvTranspose2d(self.free_parameter, self.free_parameter, kernel_size=self.kernel_sizes[6], stride=self.kernel_sizes[6], bias=False))
+        self.layers.append(nn.ReLU())
+        self.layers.append(nn.ConvTranspose2d(self.free_parameter, 1, kernel_size=self.kernel_sizes[7], stride=self.kernel_sizes[7], bias=False))
+        self.layers.append(ReshapeLayer(output_shape=self.hparams['output_shape']))           
+        # if self.hparams['outputactivation'] is not None:
+        #     self.layers.append(self.hparams['outputactivation'])
+        if self.hparams.get('NLB_outputactivation',None) is not None:
+            self.layers.append(self.hparams['NLB_outputactivation'])
+        if self.hparams.get('outputactivation',None) is not None:
+            self.layers.append(self.hparams['outputactivation'])
+
+    def forward(self, x):
+        x0 = x
+        x1 = self.layers[0](x0)
+        x2 = self.layers[1](x1)
+        x3 = self.layers[2](x2)
+        x4 = self.layers[3](x3)
+        x5 = self.layers[4](x4)
+        x6 = self.layers[5](x5)
+        x7 = self.layers[6](x6)
+        x8 = self.layers[7](x7)
+        x9 = self.layers[8](x8)
+        x10 = self.layers[9](x9)
+        x11 = self.layers[10](x10) + x9 if self.hparams['skipconnections']==True else self.layers[10](x10)
+        x12 = self.layers[11](x11)
+        x13 = self.layers[12](x12)
+        x14 = self.layers[13](x13) + x7 if self.hparams['skipconnections']==True else self.layers[13](x13)
+        x15 = self.layers[14](x14)
+        x16 = self.layers[15](x15) + x5 if self.hparams['skipconnections']==True else self.layers[15](x15)
+        x17 = self.layers[16](x16)
+        x18 = self.layers[17](x17) + x3 if self.hparams['skipconnections']==True else self.layers[17](x17)
+        x19 = self.layers[18](x18) + x1 if self.hparams['modeltype']=='model NGO' else self.layers[18](x18)
+        x20 = self.layers[19](x19)
+        y = x20
+        # if self.hparams['outputactivation'] is not None:
+        #     y = self.layers[20](y)
+        if self.hparams.get('NLB_outputactivation',None) is not None:
+            y = self.layers[20](y)
+        if self.hparams.get('outputactivation',None) is not None:
+            y = self.layers[20](y)
+        return y
+    
+
 class FNO(nn.Module):
     def __init__(self, hparams):
         super().__init__()
