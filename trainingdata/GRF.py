e2ef9bb4bec30ed0918cc3c575e05ab834cb17f7
prins <j.h.m.prins@student.tue.nl>
2024-12-04 17:37:57 +0100

    Added anisotropic GRF

--- trainingdata/GRF.py
+++ trainingdata/GRF.py
@@ -3,18 +3,19 @@ import torch
 from scipy.spatial import distance_matrix
 import opt_einsum
 
+
 class GRF:
-    def __init__(self, N_samples, d, l, device):
+    def __init__(self, N_samples, l, device):
         super().__init__()
         self.device = device
         self.N_samples = N_samples #Number of GRF samples
-        self.d = d #Dimensionality of GRF
-        self.l = l #Length scale of GRF
-        self.n_mus = int(np.ceil(max(10,1/self.l**self.d))) #Number of GRF sample locations per volume element 1/l^d
+        self.l = torch.tensor(l, device=self.device) #Length scale of GRF
+        self.d = len(l) #Dimensionality of GRF
+        self.n_mus = int(max(10,1/torch.prod(self.l))) #Number of GRF sample locations per volume element 1/l^d
         self.mus = self.compute_mus()
         self.f_hat = self.compute_RBFintcoeffs()
         self.mus = torch.tensor(self.mus, device=self.device)
-    
+
     #Sample n_samples_per_l/l^d random points on the unit square
     def compute_mus(self):
         mus = np.random.uniform(0,1,size=(self.n_mus,self.d))
@@ -22,7 +23,11 @@ class GRF:
 
     #Compute Gaussian covariance matrix cov between points
     def compute_cov(self):
-        cov = np.exp(-distance_matrix(self.mus, self.mus, p=2)**2/(2*self.l**2))
+        # cov = np.ones((self.n_mus,self.n_mus))
+        # for i in range(self.d):
+        #     cov *= np.exp(-distance_matrix(self.mus[:,i], self.mus[:,i], p=2)**2/(2*self.l[i]**2))
+        l = self.l.detach().cpu().numpy()
+        cov = np.exp(-1/2*distance_matrix(self.mus/l[None,:], self.mus/l[None,:], p=2)**2)
         return cov
     
     #Sample from a multivariate Gaussian with covariance cov
@@ -40,10 +45,14 @@ class GRF:
         f_hat = opt_einsum.contract('ij,nj->ni', cov_inv, f)
         return f_hat
     
+    def phi_n(self, i, x):
+        output = self.f_hat[i,None,:]*torch.exp(-1/2*torch.sum(((x[:,None,:] - self.mus[None,:,:])/self.l[None,None,:])**2, dim=-1))
+        return output
+    
     #Forward evaluation of RBF interpolated GRF
     def forward(self, i):
         def function(x):
-            phi_n = self.f_hat[i,None,:]*torch.exp(-torch.sum((x[:,None,:] - self.mus[None,:,:])**2, dim=-1)/(2*self.l**2))
+            phi_n = self.phi_n(i, x)
             return torch.sum(phi_n, dim=1)
         return function
 
@@ -57,25 +66,25 @@ class GRF:
     #Gradient of RBF interpolated GRF
     def grad(self, i):
         def function(x):
-            phi_n = self.f_hat[i,None,:]*torch.exp(-torch.sum((x[:,None,:] - self.mus[None,:,:])**2, axis=-1)/(2*self.l**2))
-            prefactor = -1/(self.l**2)*(x[:,None,:] - self.mus[None,:,:])
+            phi_n = self.phi_n(i, x)
+            prefactor = -1/(self.l[None,None,:]**2)*(x[:,None,:] - self.mus[None,:,:])
             return torch.sum(prefactor*phi_n[:,:,None], dim=1)
         return function
 
     #Laplacian of RBF interpolated GRF
-    def laplacian(self, i):
+    def d2dxi2(self, i):
         def function(x):
-            phi_n = self.f_hat[i,None,:]*torch.exp(-torch.sum((x[:,None,:] - self.mus[None,:,:])**2, dim=-1)/(2*self.l**2))
-            prefactor = (1/self.l**2)*(torch.sum((x[:,None,:] - self.mus[None,:,:])**2, axis=-1)/self.l**2 - self.d)
-            return torch.sum(prefactor*phi_n, dim=1)
+            phi_n = self.phi_n(i, x)
+            prefactor = (x[:,None,:] - self.mus[None,:,:])**2/self.l[None,None,:]**4 - 1/self.l[None,None,:]**2
+            return torch.sum(prefactor*phi_n[:,:,None], dim=1)
         return function
         
 
 class ScaledGRF:
-    def __init__(self, N_samples, d, l, c, b, device):
+    def __init__(self, N_samples, l, c, b, device):
         super().__init__()
         self.device = device
-        self.grf = GRF(N_samples, d, l, device)
+        self.grf = GRF(N_samples, l, device)
         #Scaling and translation of GRF f'(x) = c f(x) + b
         self.c = c
         self.b = b
@@ -95,7 +104,7 @@ class ScaledGRF:
             return self.c[i]*self.grf.grad(i)(x)
         return function
     
-    def laplacian(self, i):
+    def d2dxi2(self, i):
         def function(x):
-            return self.c[i]*self.grf.laplacian(i)(x)
+            return self.c[i]*self.grf.d2dxi2(i)(x)
         return function
